5627057ccfb0ba3eca7575eba8e34ace14c1aec4
FurryArsenic <45916013+FurryArsenic@users.noreply.github.com>
2019-06-29 13:29:09 +0200

    Add files via upload

---
+++ FizzBuzz.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+int main()
+{
+	cout << "Hello! Welcome to FizzBuzz. Please enter a value to count up to:\n";
+	int z;
+	cin >> z;
+	if (z > 0) {
+		cout << "\n";
+
+		for (int i = 1; i <= z; i++) {
+			if (i % 3 == 0) {
+				cout << "Fizz!\n";
+			}
+			else if (i % 5 == 0) {
+				cout << "Buzz!\n";
+				i++;
+			}
+			else if (i % 3 == 0 && i % 5 == 0) {
+				cout << "FizzBuzz!\n";
+			}
+			else {
+				cout << i << endl;
+			}
+		}
+	}
+	cin.get();
+}
+
